32790c7010d974ab188ecc873ffe10a36e7c31c7
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-06 23:12:00 +0900

    標準値を決める

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('id',true);
             $table->string('name');
             $table->string('email')->unique();
-            $table->longText('imgUrl');
+            $table->longText('imgUrl')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
