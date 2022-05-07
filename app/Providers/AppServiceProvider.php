a379243ede85af808ef46781b32f568ed689c25c
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-07 16:11:39 +0900

    ここでimgUrlを取り出す

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //すべてのメソッドが呼ばれる前に先に呼ばれるメソッド
+        view()->composer('*',function($view) {
+                //ビューにわたす
+                //第1引数はViewで使う時の命名
+                //第2引数はわたしたい変数or配列名
+                $imgUrl = User::select('imgUrl')
+                            ->where('id','=',\Auth::id())
+                            ->find();
+
+                $view->with('imgUrl',$imgUrl);
+            });
     }
 }
