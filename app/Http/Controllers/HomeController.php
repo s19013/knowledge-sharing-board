61f1a5a65010bc081e2137a45d8c01fdeaa96a71
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-07 16:11:13 +0900

    複数のモデルを読み込む, /myRoomが読み込まれたときの関数

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\LinkCard;
+use App\Models\Room;
+use App\Models\RoomMember;
+use App\Models\User;
 
 class HomeController extends Controller
 {
+    // imgUrlはAppServieceProvide.phpで取っている
+
     /**
      * Create a new controller instance.
      *
@@ -21,8 +27,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+
+    public function myRoom(){
+        return view('child/myRoom');
     }
 }
