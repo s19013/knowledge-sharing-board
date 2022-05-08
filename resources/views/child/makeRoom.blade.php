7e384fffcc75c9389d5746691aef8153fa4bf228
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-08 15:53:35 +0900

    部屋を作るためのフォーム

---
+++ resources/views/child/makeRoom.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="{{route('makeRoom')}}" method="POST">
+        {{-- 名前,公開非公開 --}}
+        {{-- makeRoomの関数へ --}}
+        {{-- roomNameは必ず埋める --}}
+        @csrf
+        <input type="text" name="roomName" required>
+        <input type="submit" value="作成" class="submitBtn">
+    </form>
+</div>
+@endsection
