7fc9a2b01243008dc234cf9fb9c2ea8ed091be6f
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-09 22:51:17 +0900

    検索のview

--- resources/views/child/searchRoom.blade.php
+++ resources/views/child/searchRoom.blade.php
@@ -8,24 +8,16 @@
 @endsection
 @section('content')
 <div class="container">
-    <div class="userInfBox">
-        @if (empty($userInf['imgUrl']))
-            <img class="profileImg" src="{{ asset('img/dummy_profile.png') }}" alt="">
-        @endif
-        <p class="m-0 fs-2">{{ Auth::user()->name }}</p>
-    </div>
-
-    <div class="ButtonsContena row justify-content-between">
-        <button type="button" class="makeRoomBtn btn btn-primary col-3" onclick="location.href='{{route('transitionToMakeRoom')}}'">部屋を作る</button>
-        <button type="button" class="searchRoomBtn btn btn-primary col-3" onclick="location.href=''">部屋を探す</button>
-    </div>
-
-    @foreach ($roomsUserBelongTo as $rooms)
-    <div class="roomsContena">
-        <p>{{$rooms['name']}}</p>
-        <button onclick="location.href='/room/{{$rooms['room_id']}}'">部屋に入る</button>
-    </div>
-
+    <form action="{{route('searchRoom')}}" method="get">
+        @csrf
+        <input type="text" name="searchName" required>
+        <input type="submit" class="btn btn-primary" value="探す">
+    </form>
+    @foreach ($rooms as $room)
+        <div>
+            <p>{{$room['name']}}</p>
+            <p>{{$room['owner']}}</p>
+        </div>
     @endforeach
 </div>
 @endsection
