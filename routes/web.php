60a5fa98dcd52871d973b7749776cf6e904cda86
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-09 09:34:04 +0900

    新しいルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+// post通信のときはRoute::post
 Route::get('/', [App\Http\Controllers\HomeController::class, 'myRoom'])->name('index');
 Route::get('/myRoom', [App\Http\Controllers\HomeController::class, 'myRoom'])->name('myRoom');
+Route::get('/room/{roomId}', [App\Http\Controllers\HomeController::class, 'transitionToRoom'])->name('transitionToRoom');
+Route::get('/transitionToMakeRoom', [App\Http\Controllers\HomeController::class, 'transitionToMakeRoom'])->name('transitionToMakeRoom');
+Route::post('/transitionToMakeLinkCard', [App\Http\Controllers\HomeController::class, 'transitionToMakeLinkCard'])->name('transitionToMakeLinkCard');
+Route::post('/makeRoom', [App\Http\Controllers\HomeController::class, 'makeRoom'])->name('makeRoom');
+Route::post('/makeLinkCard', [App\Http\Controllers\HomeController::class, 'makeLinkCard'])->name('makeLinkCard');
