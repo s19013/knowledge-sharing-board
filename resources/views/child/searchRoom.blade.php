04616c8f37454e3a17a349f1e095c0aa186941c9
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-10 22:19:21 +0900

    searchRoomの見た目を整えた

--- resources/views/child/searchRoom.blade.php
+++ resources/views/child/searchRoom.blade.php
@@ -4,20 +4,30 @@
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/myRoom.css') }}">
+<link rel="stylesheet" href="{{ asset('css/searchRoom.css') }}">
 @endsection
 @section('content')
 <div class="container">
     <form action="{{route('searchRoom')}}" method="get">
         @csrf
-        <input type="text" name="searchName" minlength='2' maxlength='100' required>
+        <input type="text" name="searchName" class="searchName" minlength='2' maxlength='100' required>
         <input type="submit" class="btn btn-primary" value="探す">
     </form>
-    @foreach ($rooms as $room)
-        <div>
-            <p>{{$room['name']}}</p>
-            <p>{{$room['owner']}}</p>
-        </div>
-    @endforeach
+    <div class="roomContainer border border-dark">
+        @foreach ($rooms as $room)
+            <div class="room m-1 border border-dark">
+                <div class="Card border-end border-dark m-0">
+                    <div class="cardHeader fs-4 font-weight-bold">
+                        <p class="name m-0">{{$room['name']}}</p>
+                    </div>
+                    <div class="cardBody">
+                        <p class="owner border-bottom m-0">{{$room['owner']}}</p>
+                        <p class="comment m-0">コメント</p>
+                    </div>
+                </div>
+                <button onclick="location.href='/room/{{$room['room_id']}}'">部屋に入る</button>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
