f10a8bd724fd20f560c4b00fb0b8439fb85749e2
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-10 09:44:38 +0900

    最小最大追加

--- resources/views/child/searchRoom.blade.php
+++ resources/views/child/searchRoom.blade.php
@@ -10,7 +10,7 @@
 <div class="container">
     <form action="{{route('searchRoom')}}" method="get">
         @csrf
-        <input type="text" name="searchName" required>
+        <input type="text" name="searchName" minlength='2' maxlength='100' required>
         <input type="submit" class="btn btn-primary" value="探す">
     </form>
     @foreach ($rooms as $room)
