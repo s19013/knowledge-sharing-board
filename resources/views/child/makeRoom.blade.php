8433a238e9f0b61d2cd4a4934f084c29ca5ce644
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-10 12:00:49 +0900

    見た目をととの得た｡

--- resources/views/child/makeRoom.blade.php
+++ resources/views/child/makeRoom.blade.php
@@ -2,18 +2,30 @@
 
 @section('js')
 @endsection
-
+<script>
+window.addEventListener('load', function(){
+    const $input=document.getElementById('roomNameInput');
+    const $wordCount = document.getElementById('roomNameWordCount');
+    $input.addEventListener('keyup', function () {
+        $wordCount.textContent = `残り${50 - $input.value.length}文字`;
+    })
+});
+</script>
 @section('css')
 {{-- <link rel="stylesheet" href="{{ asset('css/myRoom.css') }}"> --}}
 @endsection
 @section('content')
 <div class="container">
     <form action="{{route('makeRoom')}}" method="POST">
-        @csrf
-        <input type="text" name="roomName" minlength='2' maxlength='50' required>
-        {{-- <input type="text" name="comment" maxlength='100' required> --}}
-        {{-- 公開非公開のラジオボタン --}}
-        <input type="submit" value="作成" class="submitBtn">
+        <div class='formBlock'>
+            @csrf
+            <h2>部屋の名前 <span class="fs-6 text-danger">[必須]</span></h2>
+            <input type="text" id="roomNameInput" class="w-100 mt-2" name="roomName" placeholder="例: 〇〇の勉強方法" minlength='2' maxlength='50' required>
+            <p class="counter" id="roomNameWordCount">残り50文字</p>
+            {{-- <input type="text" name="comment" maxlength='100' required> --}}
+            {{-- 公開非公開のラジオボタン --}}
+            <input type="submit" value="作成" class="submitBtn">
+        </div>
     </form>
 </div>
 @endsection
