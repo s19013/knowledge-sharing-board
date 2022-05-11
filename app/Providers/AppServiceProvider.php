d29cb7f0f2b0c3e5f685bd748ec94b47b9adeeff
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-12 00:13:13 +0900

    paginatorでデフォルトでbootstrapを使うようにした

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        // paginatorをbootstrapで使えるらしい
+        Paginator::useBootstrap();
     }
 }
