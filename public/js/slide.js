4986e433f8ec672d2d380d774a60527be657e104
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-12 13:04:00 +0900

    不必要な部分を消した

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,12 +1,4 @@
 window.addEventListener('load', function(){
-    const $btn = document.getElementById("LeftSlideMenuBtn");
-    const $nav = document.getElementById("LeftSlideMenuNav");
-    $btn.addEventListener('click',(e) => {
-        $nav.classList.toggle('open-menu')
-    });
-});
-
-window.addEventListener('load', function(){
     const $btn = document.getElementById("RightSlideMenuBtn");
     const $nav = document.getElementById("RightSlideMenuNav");
     $btn.addEventListener('click',(e) => {
