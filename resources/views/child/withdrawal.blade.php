e0581d16e4e91b9036f3c458aa33eb4d9cfcdb08
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-13 12:38:55 +0900

    最低限の機能

---
+++ resources/views/child/withdrawal.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('js')
+
+@endsection
+
+@section('css')
+{{-- <link rel="stylesheet" href="{{ asset('css/room.css') }}"> --}}
+@endsection
+
+@section('content')
+<div class="container">
+    <p>本当に退会しますか?</p>
+    <a href=""><button >退会する</button></a>
+</div>
+@endsection
