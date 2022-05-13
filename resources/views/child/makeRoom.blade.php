62899f010b918a2bcb3793de209b52287623cfbb
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-13 16:02:26 +0900

    コメントを追加できるようにした

--- resources/views/child/makeRoom.blade.php
+++ resources/views/child/makeRoom.blade.php
@@ -9,6 +9,13 @@
             $wordCount.textContent = `残り${50 - $input.value.length}文字`;
         })
     });
+    window.addEventListener('load', function(){
+        const $input=document.getElementById('roomComentInput');
+        const $wordCount = document.getElementById('roomCommentWordCount');
+        $input.addEventListener('keyup', function () {
+            $wordCount.textContent = `残り${100 - $input.value.length}文字`;
+        })
+    });
     </script>
 @endsection
 @section('css')
@@ -22,7 +29,10 @@
             <h2>部屋の名前 <span class="fs-6 text-danger">[必須]</span></h2>
             <input type="text" id="roomNameInput" class="w-100 mt-2" name="roomName" placeholder="例: 〇〇の勉強方法" minlength='2' maxlength='50' required autofocus>
             <p class="counter" id="roomNameWordCount">残り50文字</p>
-            {{-- <input type="text" name="comment" maxlength='100' required> --}}
+
+            <h2>コメント</h2>
+            <textarea name="comment" class="w-100 mt-2" id="roomComentInput" cols="30" rows="2" maxlength='100'></textarea>
+            <p class="counter" id="roomCommentWordCount">残り100文字</p>
             {{-- 公開非公開のラジオボタン --}}
             <input type="submit" value="作成" class="submitBtn">
         </div>
